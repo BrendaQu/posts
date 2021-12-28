0e3fecde597c1859301581820e2c391582f23828
asalvero <ajalvero1@gmail.com>
2021-12-28 10:23:38 -0500

    Made changes to counter and selector

--- client/project-3-posts/src/pages/PostPage/index.js
+++ client/project-3-posts/src/pages/PostPage/index.js
@@ -1,9 +1,5 @@
 import React from 'react'
-<<<<<<< Updated upstream
-import ReactButton from '../../components/ReactButton/ReactButton'
-=======
 import ReactButton from '../../components/ReactButton';
->>>>>>> Stashed changes
 import LikeDislike from '../../components/Like_Dislike';
 import ReactCounter from '../../components/ReactCounter';
 
