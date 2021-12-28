d06cdc841957b442b8f0af5fb9f19774a0da690e
kasrayounessi <76799081+kasrayounessi@users.noreply.github.com>
2021-12-28 14:32:23 -0800

    Add files via upload

---
+++ 12-28-21/src/components/RetrievePosts/index.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import React, {useEffect, useState} from 'react';
+import Posts from './Post';
+
+const URL_FOR_POSTS = 'http://localhost:11001/postfeed/usersearch/';
+
+async function retrievePostsByUser(userId){
+    return axios.get(URL_FOR_POSTS+userId)
+    .then(response => response.data)
+    .catch(error => console.error(error))
+}
+
+const RetrievePosts = () => {
+
+    const [postsToRender, setPostsToRender] = useState([]);
+
+    const getPostsForUser = async (e) => {
+        e.preventDefault();
+        let postsRetrieved = await retrievePostsByUser(1);
+        console.log("********s")
+        console.log(postsRetrieved)
+        setPostsToRender(postsRetrieved);
+    }
+
+    return(
+        <div>
+            <button className="btn btn-block" onClick={getPostsForUser}>Click to retrieve the posts</button>
+            {
+                postsToRender.map(post => <Posts data={post} />)
+            }
+        </div>
+    )
+
+    
+    
+}
+
+export default RetrievePosts;
