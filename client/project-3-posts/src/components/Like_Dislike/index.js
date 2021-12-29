5d5355c1254795017d07d8519e65a126df11a611
Brett Davis <brett.davis5511@gmail.com>
2021-12-29 16:06:29 -0600

    Reaction Buttons

--- client/project-3-posts/src/components/Like_Dislike/index.js
+++ client/project-3-posts/src/components/Like_Dislike/index.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import mintUp from "../../imgs/mintUp.png"
 import mintDown from "../../imgs/mintDown.png"
+import mintUpUnClicked from "../../imgs/mintUpUnclicked.png"
+import mintDownUnClicked from "../../imgs/mintDownUnclicked.png"
 
 function LikeDislike() {
 
@@ -44,18 +46,19 @@ function LikeDislike() {
         }
     }
 
-    const element = (<button onClick={onClickHandler} value="cancel">---</button>)
+    const elementUp = (<input height="120" type="image" onClick={onClickHandler} value="like" src={mintUpUnClicked} draggable="false" />)
+    const elementUpClicked = (<input height="120" type="image" onClick={onClickHandler} value="cancel" src={mintUp} draggable="false" />)
+    const elementDown = (<input height="120" type="image" onClick={onClickHandler} value="dislike" src={mintDownUnClicked} draggable="false" />)
+    const elementDownClicked = (<input height="120" type="image" onClick={onClickHandler} value="cancel" src={mintDown} draggable="false" />)
 
     return (
         <div>
-            
-            <input height="75" type="image" onClick={onClickHandler} value="like" disabled={postLikes.disableLike} src={mintUp} /><br/>
-            <input height="75" type="image" onClick={onClickHandler} value="dislike" disabled={postLikes.disableDislike} src={mintDown}/><br/>
-            {postLikes.disableDislike? element : ""}
-            {postLikes.disableLike? element : ""}
+            {postLikes.disableLike? elementUpClicked : elementUp} 
+            {postLikes.disableDislike? elementDownClicked : elementDown}<br/>
             <div>{postLikes.numLikes}</div>
         </div>
     )
 }
+
 
 export default LikeDislike
--- client/project-3-posts/src/components/ReactButton/index.js
+++ client/project-3-posts/src/components/ReactButton/index.js
@@ -2,18 +2,32 @@ import React, { useState } from 'react'
 import { ReactionBarSelector } from '@charkour/react-reactions'
 
 
-const ReactButton = () => {
+const ReactButton = (props) => {
 
-    const [reaction, setReaction] = useState(false)
+    const [reaction, setReaction] = useState({
+        userId: "",
+        // postId: props.data.postId,
+        reaction: "",
+    })
 
-    function onClickHandler(){
-        setReaction(!reaction)
+    function onClickHandler(event){
+        setReaction({
+            ...reaction,
+            reaction: event.target.value
+        })
     }
 
+    console.log(reaction.reaction)
     return(
         <div>
-            <button onClick={onClickHandler}>React</button>
-            {reaction ? <ReactionBarSelector /> : ""}
+            <button onClick={onClickHandler} value="THUMBSUP">&#x1F44D;</button>
+            <button onClick={onClickHandler} value="THUMBSDOWN">&#x1F44E;</button>
+            <button onClick={onClickHandler} value="LAUGH">&#x1F923;</button>
+            <button onClick={onClickHandler} value="CRY">&#x1F622;</button>
+            <button onClick={onClickHandler} value="SMILE">&#x1F601;</button>
+            <button onClick={onClickHandler} value="FROWN">&#x1F641;</button>
+            <button onClick={onClickHandler} value="ANGRY">	&#x1F620;</button>
+            <button onClick={onClickHandler} value="HEART">	&#x1F496;</button>
         </div>
     )
 }
